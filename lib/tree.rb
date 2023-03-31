e6b848f7da22c57ff403ea1fb4f6039dec8a8c50
danielpdaniel <artofjquinn@gmail.com>
2023-03-31 12:34:46 -0400

    commit tree a613a537d20bee9cf211e8abeb858fc82903554d

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,5 +1,22 @@
+ require "pry"
+
 class Tree
   def initialize(root = nil)
     @root = root
+  end
+
+  def get_element_by_id(string)
+    nodes_to_search = [@root]
+    results = []
+
+    while nodes_to_search.length > 0
+      current_node = nodes_to_search.shift
+      results.push(current_node[:tag_name])
+      if current_node[:id] == string
+        return current_node
+      else
+      nodes_to_search = nodes_to_search + current_node[:children]
+      end
+    end
   end
 end
